68e0be389b8faf1ae48d465b74a17d97cce51c1e
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2026-01-21 04:32:34 +0000

    fix

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -22,7 +22,7 @@ public class PracticeProblem {
 			swapped = false;
 			for (int i = 1; i < nums.length - j; i++) {
 				steps++;
-				if( nums[i -1] < nums[i]) {
+				if( nums[i -1] > nums[i]) {
 					double temp = nums[i-1];
 					nums[i - 1] = nums[i];
 					nums[i] = temp;
@@ -41,7 +41,7 @@ public class PracticeProblem {
 			int largest = j;
 			for (int i = j + 1; i < nums.length; i ++) {
 				steps++;
-				if(nums[i] < nums[largest]) {
+				if(nums[i] > nums[largest]) {
 					largest = i;
 				}
 			}
@@ -61,7 +61,7 @@ public class PracticeProblem {
 			double key = nums[i];
 			int index = i - 1;
 
-			while(index >= 0 && nums[index] < key) {
+			while(index >= 0 && nums[index] > key) {
 				steps++;
 				nums[index + 1] = nums[index];
 				swaps += 1;
@@ -88,7 +88,7 @@ public class PracticeProblem {
 		if (insertion == min) return "Insertion";
 		return "Selection";
 	}
-	public static String leastSteps(double[] nums) {
+	public static String leastIterations(double[] nums) {
 		int[] b = bubbleSort(copyArray(nums));
 		int[] s = selectionSort(copyArray(nums));
 		int[] i = insertionSort(copyArray(nums));
